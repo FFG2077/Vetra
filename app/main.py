587eba0129156f95204240b85aa967e91c01ccb3
ffg <ffg@ffgs-MacBook-Pro.local>
2025-10-28 15:08:38 +0300

    feat(auth): add custom OpenAPI schema with bearerAuth

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from api.v1 import api_router
 from infrastructure.database import Base, engine
+from fastapi.openapi.utils import get_openapi
 
 
 # create tables
@@ -10,3 +11,34 @@ app = FastAPI(title='Seetalk')
 
 # routes
 app.include_router(api_router)
+
+
+def custom_openapi():
+	if app.openapi_schema:
+		return app.openapi_schema
+	
+	openapi_schema = get_openapi(
+		title="Seetalk API",
+		version="1.0.0",
+		routes=app.routes,
+	)
+	# add securityScheme
+	openapi_schema["components"]["securitySchemes"] = {
+    "bearerAuth": {
+      "type": "http",
+      "scheme": "bearer",
+      "bearerFormat": "JWT",
+    }
+  }
+
+	# apply to all endpoints by default
+	for path in openapi_schema["paths"].values():
+		for method in path.values():
+			method["security"] = [{"bearerAuth": []}]
+
+	app.openapi_schema = openapi_schema
+
+	return app.openapi_schema
+
+
+app.openapi = custom_openapi
