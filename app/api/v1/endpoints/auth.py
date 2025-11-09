33a46c0c8161811e5f489892f2db90a31a96394e
ffg <ffg@ffgs-MacBook-Pro.local>
2025-11-09 17:05:29 +0300

    cleanup: remove extra commented code

--- app/api/v1/endpoints/auth.py
+++ app/api/v1/endpoints/auth.py
@@ -27,22 +27,6 @@ async def registration(creds: UserRegisterSchema, response: Response, db: AsyncS
 	repo = UserRepository(db)
 	service = UserService(repo)
 	user = await service.register_user(creds)
-	# check if mail is busy
-	# result = await db.execute(select(User).where(User.email == creds.email))
-	# user = result.scalars().first()
-	# if user:
-	# 	raise HTTPException(status_code=400, detail='Email already exists')
-	
-	# # create user
-	# user = User(
-	# 	name=creds.name,
-	# 	email=creds.email,
-	# 	password_hash=hash_password(creds.password)
-	# )
-	
-	# db.add(user)
-	# await db.commit()
-	# await db.refresh(user)
 
 	token = security.create_access_token(uid=str(user.id))
 	
