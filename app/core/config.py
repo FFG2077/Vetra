3e5caef5eeb45a97282f3050f682a343b9434a29
ffg <t3st.py@gmail.com>
2026-06-19 21:21:24 +0300

    fix: stabilize env config and resolve startup issues

--- app/core/config.py
+++ app/core/config.py
@@ -1,5 +1,8 @@
-from pydantic_settings import BaseSettings
+from pydantic_settings import BaseSettings, SettingsConfigDict
 from functools import lru_cache
+from pathlib import Path
+
+env_path = Path(__file__).resolve().parents[2] / ".env"
 
 
 class Settings(BaseSettings):
@@ -8,17 +11,20 @@ class Settings(BaseSettings):
 
 	# DataBase
 	DATABASE_URL: str
-
+	MYSQL_ROOT_PASSWORD: str
+	MYSQL_DATABASE: str
+	MYSQL_USER: str
+	MYSQL_PASSWORD: str
 	# AuthX
 	JWT_SECRET_KEY: str
 
 	# For debug
 	DEBUG: bool
 
-
-	class Config:
-		env_file = '.env'
-		case_sensitive = True
+	model_config = SettingsConfigDict(env_file=env_path, case_sensitive=True)
+	# class Config:
+	# 	env_file = '.env'
+	# 	case_sensitive = True
 
 
 @lru_cache()
